d651f0243d5fbe6864667d70960d505ec94c6ec1
Erickeyz <yunweneric@gmail.com>
2022-08-24 18:51:17 +0100

    added theme

--- lib/start/onboarding_screen.dart
+++ lib/start/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class OnboadingScreen extends StatefulWidget {
@@ -8,8 +9,45 @@ class OnboadingScreen extends StatefulWidget {
 }
 
 class _OnboadingScreenState extends State<OnboadingScreen> {
+  final PageController controller = PageController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(EvaIcons.arrowBack),
+                    Text("Skp"),
+                  ],
+                ),
+                Image.asset("assets/images/illus3.png"),
+                Text("Buy Tickets"),
+                Text("Get your tickets"),
+                Row(
+                  children: [
+                    Container(),
+                    CircleAvatar(
+                      child: Icon(EvaIcons.arrowRight),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Center(
+            child: Text('Second Page'),
+          ),
+          Center(
+            child: Text('Third Page'),
+          )
+        ],
+      ),
+    );
   }
 }
